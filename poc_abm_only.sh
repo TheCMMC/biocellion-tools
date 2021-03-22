71d328311f6d12f25847799ea29c1777b9cf587d
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2021-03-22 17:36:49 +0100

    delete the parameter line

--- poc_abm_only.sh
+++ poc_abm_only.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 BIOCELLION_MODEL_PATH="/home/jaroknor/NLeSC/InSilicoMeat/Biocellion/Biocellion-3.1/biocellion-3.1/biocellion-user/ABM-only-microcarriers"
-PARAMETER_FILE="${BIOCELLION_MODEL_PATH}/Parameters-ABM-Only.csv"
 
 #extract the parameters from the spreadsheet
 SKIP=4
