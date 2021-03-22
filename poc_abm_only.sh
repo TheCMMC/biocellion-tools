1aa830d5cce7d08e51ac8b0707a22e4957a5f526
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2021-03-22 22:21:17 +0100

    fix running make in wrong directory

--- poc_abm_only.sh
+++ poc_abm_only.sh
@@ -18,9 +18,9 @@ echo "array of TRESHOLD : ${TRESH[@]}"
 echo "array of MECH_TRESHOLD : ${MECHTRESHD[@]}"
 echo "array of TIME_DEATH : ${TDEATH[@]}"
 
-for ((Y=4;Y<=6;Y++));
+for ((Y=3;Y<=4;Y++));
 do
-    for ((Z=1;Z<=16;Z++));
+    for ((Z=17;Z<=23;Z++));
     do
         INDEX=$(expr $Z - 1)
         SIM_SET="parameter${IDS[$INDEX]}_trial${Y}"
@@ -36,6 +36,7 @@ do
             echo -e "$SIM_SET ran to completion \n"
 
         else
+            cd $BIOCELLION_MODEL_PATH
             make clean
             echo "Prepare simulations for id = ${IDS[$INDEX]}, trial = $Y"
             echo "with parameters: hill = ${HILL[$INDEX]}, treshold = ${TRESH[$INDEX]}, mech_treshold = ${MECHTRESHD[$INDEX]}, time_to_death = ${TDEATH[$INDEX]}"
@@ -44,19 +45,20 @@ do
             sed -i "s/const REAL STRESS_HILL_EXPONENT = .\+/const REAL STRESS_HILL_EXPONENT = ${HILL[$INDEX]} ;/g" ${MODEL_DEFINE}
             sed -i "s/const REAL STRESS_TRESHOLD = .\+/const REAL STRESS_TRESHOLD = ${TRESH[$INDEX]} ;/g" ${MODEL_DEFINE}
             sed -i "s/const REAL MECH_STRESS_TRESHOLD_DEATH = .\+/const REAL MECH_STRESS_TRESHOLD_DEATH = ${MECHTRESHD[$INDEX]} ;/g" ${MODEL_DEFINE}
-            sed -i "s/const REAL TIME_TO_DEATH = .\+/const REAL TIME_TO_DEATH = ${TDEATH[$INDEX]} * DOUBLING_TIME ;/g" ${MODEL_DEFINE}
+            sed -i "s/const REAL TIME_TO_DEATH = .\+/const REAL TIME_TO_DEATH = ${TDEATH[$INDEX]};/g" ${MODEL_DEFINE}
             sed -i "s@<output path=.\+@<output path=\"${OUT_DIR}\" particle=\"pvtp\" grid=\"vthb\" start_x=\"0\" start_y=\"0\" start_z=\"0\" size_x=\"4\" size_y=\"4\" size_z=\"4\"/>@g" ${RUN_MODEL}
             sed -i "s@<stdout path=.\+@<stdout path=\"${OUT_DIR}\" verbosity=\"3\" time_stamp=\"yes\"/>@g" ${RUN_MODEL}
             
             # build the biocellion model
-            # make -s
+            make -s 
 
             echo "Begin simulation for $OUT_DIR"
-            # biocellion run_model.xml
-            echo -e "\n End simulation for $OUT_DIR"
+            biocellion "${BIOCELLION_MODEL_PATH}/run_model.xml"
+            echo -e "End simulation for $OUT_DIR \n"
 
             #rename the summary file to the proper filename
             mv "${OUT_DIR}/output.log" "${OUT_DIR}/output_${SIM_SET}.txt"
+            cd -
         fi
     done
 done
