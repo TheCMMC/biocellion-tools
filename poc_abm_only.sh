5475cecff121e9bba8e57a75a27e93b9ba43dd9a
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2021-03-22 17:35:36 +0100

    terrible terrible

--- poc_abm_only.sh
+++ poc_abm_only.sh
@@ -5,12 +5,5 @@ PARAMETER_FILE="${BIOCELLION_MODEL_PATH}/Parameters-ABM-Only.csv"
 
 #extract the parameters from the spreadsheet
 SKIP=4
-IDS=( $(tai
-            # biocellion run_model.xml
-            echo -e "\n End simulation for $OUT_DIR"
-
-            #rename the summary file to the proper filename
-            mv "${OUT_DIR}/output.log" "${OUT_DIR}/output_${SIM_SET}.txt"
-        fi
     done
 done
