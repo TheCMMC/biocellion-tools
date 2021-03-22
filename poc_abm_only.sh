87d8d2ffb84eb37e8936108b56732c815ebf1f57
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2021-03-22 17:32:51 +0100

    destroy even further

--- poc_abm_only.sh
+++ poc_abm_only.sh
@@ -5,23 +5,7 @@ PARAMETER_FILE="${BIOCELLION_MODEL_PATH}/Parameters-ABM-Only.csv"
 
 #extract the parameters from the spreadsheet
 SKIP=4
-IDS=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f1) )
-HILL=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f2) )
-TRESH=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f3) )
-MECHTRESHD=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f4) )
-TDEATH=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f5) )
-
-# # check whether arrays have been substracted properly, run this piece first before you execute the script
-echo "array of ParamIDs  : ${IDS[@]}"
-echo "array of HILL_EXP   : ${HILL[@]}"
-echo "array of TRESHOLD : ${TRESH[@]}"
-echo "array of MECH_TRESHOLD : ${MECHTRESHD[@]}"
-echo "array of TIME_DEATH : ${TDEATH[@]}"
-
-for ((Y=4;Y<=6;Y++));
-do
-    for ((Z=1;Z<=16;Z++));
-    
+IDS=( $(tai
             # biocellion run_model.xml
             echo -e "\n End simulation for $OUT_DIR"
 
