c37986b02f162fddbe36e3cb3136b186b7fbd328
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2021-03-22 17:14:51 +0100

    Read parameters from csv for bash script, add documentation to notebook

--- poc_abm_only.sh
+++ poc_abm_only.sh
@@ -1,33 +1,62 @@
-HILL_LIST=(1.0 2.0 3.0 5.0)
-TRESHOLD_LIST=(1.0 1e-6 1e-7 1e-8)
+#! /bin/bash
+
+BIOCELLION_MODEL_PATH="/home/jaroknor/NLeSC/InSilicoMeat/Biocellion/Biocellion-3.1/biocellion-3.1/biocellion-user/ABM-only-microcarriers"
+PARAMETER_FILE="${BIOCELLION_MODEL_PATH}/Parameters-ABM-Only.csv"
+
+#extract the parameters from the spreadsheet
+SKIP=4
+IDS=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f1) )
+HILL=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f2) )
+TRESH=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f3) )
+MECHTRESHD=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f4) )
+TDEATH=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f5) )
+
+# # check whether arrays have been substracted properly, run this piece first before you execute the script
+echo "array of ParamIDs  : ${IDS[@]}"
+echo "array of HILL_EXP   : ${HILL[@]}"
+echo "array of TRESHOLD : ${TRESH[@]}"
+echo "array of MECH_TRESHOLD : ${MECHTRESHD[@]}"
+echo "array of TIME_DEATH : ${TDEATH[@]}"
+
 for ((Y=4;Y<=6;Y++));
 do
-    for ((X=1;X<=16;X++));
+    for ((Z=1;Z<=16;Z++));
     do
-        OUT_DIR="./output_parameter${X}_trial${Y}"
-        LOG_DIR="./log_parameter${X}_trial${Y}"
-        if [ "$(ls -A $OUT_DIR)" ]; 
+        INDEX=$(expr $Z - 1)
+        SIM_SET="parameter${IDS[$INDEX]}_trial${Y}"
+        OUT_DIR="${BIOCELLION_MODEL_PATH}/output/output_${SIM_SET}"
+        LOG_DIR="${BIOCELLION_MODEL_PATH}/output/"
+
+        MODEL_DEFINE="${BIOCELLION_MODEL_PATH}/model_define.h" 
+        RUN_MODEL="${BIOCELLION_MODEL_PATH}/run_model.xml"
+
+        mkdir -p $OUT_DIR
+        if [ "$(ls -A $OUT_DIR)" ] && $(grep -q "75000/75000" "${OUT_DIR}/output_${SIM_SET}.txt")
         then
-            echo "$OUT_DIR is already full"
+            echo -e "$SIM_SET ran to completion \n"
+
         else
             make clean
+            echo "Prepare simulations for id = ${IDS[$INDEX]}, trial = $Y"
+            echo "with parameters: hill = ${HILL[$INDEX]}, treshold = ${TRESH[$INDEX]}, mech_treshold = ${MECHTRESHD[$INDEX]}, time_to_death = ${TDEATH[$INDEX]}"
+
+            # prepare biocellion files with new parameters
+            sed -i "s/const REAL STRESS_HILL_EXPONENT = .\+/const REAL STRESS_HILL_EXPONENT = ${HILL[$INDEX]} ;/g" ${MODEL_DEFINE}
+            sed -i "s/const REAL STRESS_TRESHOLD = .\+/const REAL STRESS_TRESHOLD = ${TRESH[$INDEX]} ;/g" ${MODEL_DEFINE}
+            sed -i "s/const REAL MECH_STRESS_TRESHOLD_DEATH = .\+/const REAL MECH_STRESS_TRESHOLD_DEATH = ${MECHTRESHD[$INDEX]} ;/g" ${MODEL_DEFINE}
+            sed -i "s/const REAL TIME_TO_DEATH = .\+/const REAL TIME_TO_DEATH = ${TDEATH[$INDEX]} * DOUBLING_TIME ;/g" ${MODEL_DEFINE}
+            sed -i "s@<output path=.\+@<output path=\"${OUT_DIR}\" particle=\"pvtp\" grid=\"vthb\" start_x=\"0\" start_y=\"0\" start_z=\"0\" size_x=\"4\" size_y=\"4\" size_z=\"4\"/>@g" ${RUN_MODEL}
+            sed -i "s@<stdout path=.\+@<stdout path=\"${OUT_DIR}\" verbosity=\"3\" time_stamp=\"yes\"/>@g" ${RUN_MODEL}
             
-            HILL_INDEX=$(((X-1) / 4))
-            TRESHOLD_INDEX=$(((X-1) % 4))
-            HILL=${HILL_LIST[HILL_INDEX]}
-            TRESHOLD=${TRESHOLD_LIST[TRESHOLD_INDEX]}
-            
-            echo "Prepare simulations with id = ${X}, hill = ${HILL}, treshold = ${TRESHOLD}"
-            sed -i "s/const REAL STRESS_HILL_EXPONENT = .\+/const REAL STRESS_HILL_EXPONENT = ${HILL} ;/g" ./model_define.h
-            sed -i "s/const REAL STRESS_TRESHOLD = .\+/const REAL STRESS_TRESHOLD = ${TRESHOLD} ;/g" ./model_define.h
-            sed -i "s@<output path=.\+@<output path=\"${OUT_DIR}\" particle=\"pvtp\" grid=\"vthb\" start_x=\"0\" start_y=\"0\" start_z=\"0\" size_x=\"4\" size_y=\"4\" size_z=\"4\"/>@g" ./run_model.xml
-            sed -i "s@<stdout path=.\+@<stdout path=\"${LOG_DIR}\" verbosity=\"3\" time_stamp=\"yes\"/>@g" ./run_model.xml
-            
-            make -s
+            # build the biocellion model
+            # make -s
 
             echo "Begin simulation for $OUT_DIR"
-            biocellion run_model.xml
-            printf "\n"
+            # biocellion run_model.xml
+            echo -e "\n End simulation for $OUT_DIR"
+
+            #rename the summary file to the proper filename
+            mv "${OUT_DIR}/output.log" "${OUT_DIR}/output_${SIM_SET}.txt"
         fi
     done
 done
