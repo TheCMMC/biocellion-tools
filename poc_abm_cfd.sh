83431cc2129ececa11ee993b81aa3bb667460543
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2021-03-26 21:47:35 +0100

    correct regex for rpm replacement

--- poc_abm_cfd.sh
+++ poc_abm_cfd.sh
@@ -16,19 +16,19 @@ RPM=( $(tail -n +$SKIP ${PARAMETER_FILE} | cut -d ',' -f4) )
 
 # check whether arrays have been extracted properly, run this piece first before you execute the rest of the script
 echo "array of ParamIDs  : ${IDS[@]}"
-echo "array of HILL_EXP   : ${HILL[@]}"
 echo "array of TRESHOLD : ${TRESH[@]}"
 echo "array of MECH_TRESHOLD : ${MECHTRESHD[@]}"
-echo "array of TIME_DEATH : ${TDEATH[@]}"
+echo "array of RPM : ${RPM[@]}"
 
 LOG_DIR="${BIOCELLION_MODEL_PATH}/output/"
 MODEL_DEFINE="${BIOCELLION_MODEL_PATH}/model_define.h" 
+MODEL_CONFIG="${BIOCELLION_MODEL_PATH}/model_routine_config.cpp" 
 RUN_MODEL="${BIOCELLION_MODEL_PATH}/run_model.xml"
 
 # Set the values for Y (trial) and Z (parameter ID) in the for loops to specify the range of simulations to run
-for ((Y=4;Y<=6;Y++));
+for ((Y=4;Y<=4;Y++));
 do
-    for ((Z=5;Z<=18;Z++));
+    for ((Z=5;Z<=7;Z++));
     do
         # list indexing starts at 0, while parameter IDs start at 1, set simulation specific paths and create output directory
         INDEX=$(expr $Z - 1)
@@ -42,15 +42,15 @@ do
             echo -e "$SIM_SET ran to completion \n"
 
         else
-            # cd $BIOCELLION_MODEL_PATH
+            cd $BIOCELLION_MODEL_PATH
             # make clean
             echo "Prepare simulations for id = ${IDS[$INDEX]}, trial = $Y"
-            echo "with parameters: hill = ${HILL[$INDEX]}, treshold = ${TRESH[$INDEX]}, mech_treshold = ${MECHTRESHD[$INDEX]}, time_to_death = ${TDEATH[$INDEX]}"
+            echo "with parameters: treshold = ${TRESH[$INDEX]}, mech_treshold = ${MECHTRESHD[$INDEX]}, rpm = ${RPM[$INDEX]}"
 
             # prepare biocellion files with new parameters
             sed -i "s/const REAL STRESS_TRESHOLD = .\+/const REAL STRESS_TRESHOLD = ${TRESH[$INDEX]} ;/g" ${MODEL_DEFINE}
             sed -i "s/const REAL MECH_STRESS_TRESHOLD_DEATH = .\+/const REAL MECH_STRESS_TRESHOLD_DEATH = ${MECHTRESHD[$INDEX]} ;/g" ${MODEL_DEFINE}
-            sed -i "s/  cfd_setup((char*)\"Velocity_.\+/  cfd_setup((char*)\"Velocity_${RPM[$INDEX]}rpm.txt\") ;/g" ${MODEL_DEFINE}
+            sed -i "s/  cfd_setup((char\*)\"Velocity.\+/  cfd_setup((char\*)\"Velocity_${RPM[$INDEX]}rpm.txt\") ;/g" ${MODEL_CONFIG}
             sed -i "s@<output path=.\+@<output path=\"${OUT_DIR}\" particle=\"pvtp\" grid=\"vthb\" start_x=\"0\" start_y=\"0\" start_z=\"0\" size_x=\"4\" size_y=\"4\" size_z=\"4\"/>@g" ${RUN_MODEL}
             sed -i "s@<stdout path=.\+@<stdout path=\"${OUT_DIR}\" verbosity=\"3\" time_stamp=\"yes\"/>@g" ${RUN_MODEL}
             
